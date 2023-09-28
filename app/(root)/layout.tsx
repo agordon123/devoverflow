9728ef3bd9eb6857a452cc6262ed519b723927e2
adam <adamg42685@gmail.com>
2023-09-28 10:38:43 -0400

    adding assets, navbar, authentication

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,43 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter, Space_Grotesk } from "next/font/google";
+import React from "react";
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-inter",
+});
+
+export const metadata: Metadata = {
+  title: "Dev Overflow",
+  description: "Community Platform for Software Developers",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: `primary-text-gradient hover:text-primary-500`,
+        },
+      }}
+    >
+      <html lang="en">
+        <body className={`${inter.className} ${spaceGrotesk.className}`}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <main className="flex-center">
       <h1 className="h1-bold">Next JS 13 We&apos;re Coming!</h1>
+      <UserButton afterSignOutUrl="/" />
     </main>
   );
 }
