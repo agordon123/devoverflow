58d90742e908636d245f0a0a028a8af5e3f302a4
adam <adamg42685@gmail.com>
2023-10-04 18:04:53 -0400

    navbar and theme context provider working

---
+++ app/layout.tsx
@@ -0,0 +1,44 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter, Space_Grotesk } from "next/font/google";
+import React from "react";
+import { ThemeProvider } from "@/context/ThemeProvider";
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-inter",
+});
+
+export const metadata: Metadata = {
+  title: "Dev Overflow",
+  description: "Community Platform for Software Developers",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} ${spaceGrotesk.className}`}>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: "primary-gradient",
+              footerActionLink: `primary-text-gradient hover:text-primary-500`,
+            },
+          }}
+        >
+          <ThemeProvider>{children}</ThemeProvider>{" "}
+        </ClerkProvider>
+      </body>
+    </html>
+  );
+}
