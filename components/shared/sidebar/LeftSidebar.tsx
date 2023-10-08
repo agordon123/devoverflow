fccfc35a6b6261322a97544ab20748057f83b90e
adam <adamg42685@gmail.com>
2023-10-08 14:46:48 -0400

    sidebars are now working ```

---
+++ components/shared/sidebar/LeftSidebar.tsx
@@ -0,0 +1,90 @@
+"use client";
+import React from "react";
+import { sidebarLinks } from "@/constants";
+import Link from "next/link";
+import { SignedOut, useAuth } from "@clerk/nextjs";
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+// the usePathname() hook if the link of a particular item is the same as the current URL we will get from the Next.js Hook!
+const LeftSidebar = () => {
+  const pathname = usePathname();
+  const { userId } = useAuth();
+
+  // top level css classes - custom-scrollbar , background light/dark , light border, sticky (to keep on left)
+  // left - 0 border top 0 border flex-h-screen flex-col justify-between overflow-y-auto   p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]
+  //
+  return (
+    <section className="custom-scrollbar background-light900_dark200 light-border sticky left-0 top-0 flex h-screen w-fit flex-col justify-between  overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden xl:w-[266px]">
+      <div className="flex flex-1 flex-col gap-6">
+        {sidebarLinks.map((item) => {
+          const isActive =
+            (pathname.includes(item.route) && item.route.length > 1) ||
+            pathname === item.route;
+          if (item.route === "/profile") {
+            if (userId) {
+              item.route = `${item.route}/${userId}`;
+            } else {
+              return null;
+            }
+          }
+          return (
+            <Link
+              href={item.route}
+              key={item.label}
+              className={`${
+                isActive
+                  ? "primary-gradient rounded-lg text-light-900"
+                  : "text-dark300_light900"
+              }
+          
+            : flex items-center justify-start gap-4 bg-transparent p-4`}
+            >
+              <Image
+                src={item.imgURL}
+                alt={item.label}
+                height={20}
+                width={20}
+                className={`${isActive ? "" : "invert-colors"}`}
+              />
+              {item.label}
+            </Link>
+          );
+        })}
+        <SignedOut>
+          <div className="flex flex-col gap-3">
+            <Link href="/sign-in">
+              <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                {" "}
+                <Image
+                  src="/assets/icons/account.svg"
+                  alt="login"
+                  width={20}
+                  height={20}
+                  className="invert-colors lg:hidden"
+                />
+                <span className="primary-text-gradient max-lg:hidden">
+                  Log In
+                </span>
+              </Button>
+            </Link>
+            <Link href="/sign-up">
+              <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
+                <Image
+                  src="/assets/icons/sign-up.svg"
+                  alt="sign up"
+                  width={20}
+                  height={20}
+                  className="invert-colors lg:hidden"
+                />
+                <span className="max-lg:hidden">Sign up</span>
+              </Button>
+            </Link>
+          </div>
+        </SignedOut>
+      </div>
+    </section>
+  );
+};
+
+export default LeftSidebar;
