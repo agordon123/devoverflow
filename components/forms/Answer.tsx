d9ba17645a33d80524bf2bdb723cf8652f861e68
adam <adamg42685@gmail.com>
2023-11-02 13:49:29 -0400

    updating answer form

--- components/forms/Answer.tsx
+++ components/forms/Answer.tsx
@@ -1,17 +1,18 @@
 "use client";
-import React, { useRef } from "react";
-import { zodResolver } from "@hookform/resolvers/zod";
-import * as z from "zod";
-import { AnswerSchema } from "@/lib/validation";
-import { useForm } from "react-hook-form";
+
 import {
   Form,
   FormControl,
   FormField,
   FormItem,
   FormMessage,
-} from "@/components/ui/form";
+} from "../ui/form";
+import { useForm } from "react-hook-form";
+import { AnswerSchema } from "@/lib/validation";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Editor } from "@tinymce/tinymce-react";
+import { useRef, useState } from "react";
 import { useTheme } from "@/context/ThemeProvider";
 import { Button } from "../ui/button";
 import Image from "next/image";
@@ -19,17 +20,16 @@ import { createAnswer } from "@/lib/actions/answer.actions";
 import { usePathname } from "next/navigation";
 
 interface Props {
-  questionId: string;
   question: string;
+  questionId: string;
   authorId: string;
 }
-const Answer = ({ questionId, question, authorId }: Props) => {
-  const [isSubmitting, setIsSubmitting] = React.useState(false);
-  const editorRef = useRef(null);
 
-  const { mode } = useTheme();
+const Answer = ({ question, questionId, authorId }: Props) => {
   const pathname = usePathname();
-
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const { mode } = useTheme();
+  const editorRef = useRef(null);
   const form = useForm<z.infer<typeof AnswerSchema>>({
     resolver: zodResolver(AnswerSchema),
     defaultValues: {
@@ -39,6 +39,7 @@ const Answer = ({ questionId, question, authorId }: Props) => {
 
   const handleCreateAnswer = async (values: z.infer<typeof AnswerSchema>) => {
     setIsSubmitting(true);
+
     try {
       await createAnswer({
         content: values.answer,
@@ -46,9 +47,12 @@ const Answer = ({ questionId, question, authorId }: Props) => {
         question: JSON.parse(questionId),
         path: pathname,
       });
+
       form.reset();
+
       if (editorRef.current) {
         const editor = editorRef.current as any;
+
         editor.setContent("");
       }
     } catch (error) {
@@ -57,24 +61,33 @@ const Answer = ({ questionId, question, authorId }: Props) => {
       setIsSubmitting(false);
     }
   };
+
   return (
     <div>
       <div className="flex flex-col justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
-        <h4>Write your answer here</h4>
-        <Button className="btn light-border-2 gap-1.5 rounded-md px-4 py-2.5 text-primary-500 shadow-none dark:text-primary-500">
+        <h4 className="paragraph-semibold text-dark400_light800">
+          Write your answer here
+        </h4>
+
+        <Button
+          className="btn light-border-2 gap-1.5 rounded-md px-4 py-2.5 text-primary-500 shadow-none dark:text-primary-500"
+          onClick={() => {}}
+        >
           <Image
             src="/assets/icons/stars.svg"
-            alt="stars"
+            alt="star"
             width={12}
             height={12}
+            className="object-contain"
           />
           Generate an AI Answer
         </Button>
       </div>
+
       <Form {...form}>
         <form
-          onSubmit={form.handleSubmit(handleCreateAnswer)}
           className="mt-6 flex w-full flex-col gap-10"
+          onSubmit={form.handleSubmit(handleCreateAnswer)}
         >
           <FormField
             control={form.control}
@@ -90,7 +103,6 @@ const Answer = ({ questionId, question, authorId }: Props) => {
                     }}
                     onBlur={field.onBlur}
                     onEditorChange={(content) => field.onChange(content)}
-                    initialValue=""
                     init={{
                       height: 350,
                       menubar: false,
@@ -126,6 +138,7 @@ const Answer = ({ questionId, question, authorId }: Props) => {
               </FormItem>
             )}
           />
+
           <div className="flex justify-end">
             <Button
               type="submit"
