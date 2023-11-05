d9be6ace889d612d603848b0ae4a0fb118db70c6
adam <adamg42685@gmail.com>
2023-11-05 18:02:27 -0500

    missed user actions

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,34 +1,23 @@
-/* eslint-disable no-unused-vars */
 "use server";
 
+import { FilterQuery } from "mongoose";
 import User from "@/database/user.model";
-import Tag from "@/database/tags.model";
 import { connectToDb } from "../mongoose";
 import {
   CreateUserParams,
   DeleteUserParams,
   GetAllUsersParams,
-  GetUserByIdParams,
-  UpdateUserParams,
   GetSavedQuestionsParams,
+  GetUserByIdParams,
   ToggleSaveQuestionParams,
+  UpdateUserParams,
 } from "./shared.types";
 import { revalidatePath } from "next/cache";
 import Question from "@/database/question.model";
-import { FilterQuery } from "mongoose";
+import Tag from "@/database/tags.model";
+import Answer from "@/database/answer.model";
 
-export const getAllUsers = async (params: GetAllUsersParams) => {
-  try {
-    connectToDb();
-    const { page, pageSize, filter, searchQuery } = params;
-    const users = await User.find({}).sort({ createdAt: -1 });
-    return { users };
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-};
-export const getUserById = async (params: GetUserByIdParams): Promise<any> => {
+export async function getUserById(params: any) {
   try {
     connectToDb();
 
@@ -41,26 +30,38 @@ export const getUserById = async (params: GetUserByIdParams): Promise<any> => {
     console.log(error);
     throw error;
   }
-};
-export async function createUser(params: CreateUserParams) {
+}
+
+export async function createUser(userData: CreateUserParams) {
   try {
     connectToDb();
-    const newUser = await User.create(params);
+
+    const newUser = await User.create(userData);
+
     return newUser;
   } catch (error) {
     console.log(error);
+    throw error;
   }
 }
+
 export async function updateUser(params: UpdateUserParams) {
   try {
     connectToDb();
+
     const { clerkId, updateData, path } = params;
-    await User.findOneAndUpdate({ clerkId }, updateData, { new: true });
+
+    await User.findOneAndUpdate({ clerkId }, updateData, {
+      new: true,
+    });
+
     revalidatePath(path);
   } catch (error) {
     console.log(error);
+    throw error;
   }
 }
+
 export async function deleteUser(params: DeleteUserParams) {
   try {
     connectToDb();
@@ -77,9 +78,7 @@ export async function deleteUser(params: DeleteUserParams) {
     // and questions, answers, comments, etc.
 
     // get user question ids
-    const userQuestionIds = await Question.find({ author: user._id }).distinct(
-      "_id"
-    );
+    // const userQuestionIds = await Question.find({ author: user._id}).distinct('_id');
 
     // delete user questions
     await Question.deleteMany({ author: user._id });
@@ -94,6 +93,22 @@ export async function deleteUser(params: DeleteUserParams) {
     throw error;
   }
 }
+
+export async function getAllUsers(params: GetAllUsersParams) {
+  try {
+    connectToDb();
+
+    // const { page = 1, pageSize = 20, filter, searchQuery } = params;
+
+    const users = await User.find({}).sort({ createdAt: -1 });
+
+    return { users };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
 export async function toggleSaveQuestion(params: ToggleSaveQuestionParams) {
   try {
     connectToDb();
@@ -135,6 +150,7 @@ export async function getSavedQuestions(params: GetSavedQuestionsParams) {
   try {
     connectToDb();
 
+    // eslint-disable-next-line no-unused-vars
     const { clerkId, page = 1, pageSize = 10, filter, searchQuery } = params;
 
     const query: FilterQuery<typeof Question> = searchQuery
@@ -165,3 +181,36 @@ export async function getSavedQuestions(params: GetSavedQuestionsParams) {
     throw error;
   }
 }
+export async function getUserInfo(params: GetUserByIdParams) {
+  try {
+    connectToDb();
+
+    const { userId } = params;
+
+    const user = await User.findOne({ clerkId: userId });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const totalQuestions = await Question.countDocuments({ author: user._id });
+    const totalAnswers = await Answer.countDocuments({ author: user._id });
+    console.log(user, "user", totalAnswers, totalQuestions);
+    return {
+      user,
+      totalQuestions,
+      totalAnswers,
+    };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+// export async function getAllUsers(params: GetAllUsersParams) {
+//   try {
+//     connectToDb();
+//   } catch (error) {
+//     console.log(error);
+//     throw error;
+//   }
+// }
