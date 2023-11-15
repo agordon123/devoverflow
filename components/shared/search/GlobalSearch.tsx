72c923dc64269eae7b477018831f2819ba57e6cd
adam <adamg42685@gmail.com>
2023-11-14 22:01:04 -0500

    global search working

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -1,8 +1,10 @@
 "use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Input } from "@/components/ui/input";
-
+import { useRouter, useSearchParams, usePathname } from "next/navigation";
+import { removeKeysFromQuery, formUrlQuery } from "@/lib/utils";
+import GlobalResult from "./GlobalResult";
 interface Props {
   filters: {
     name: string;
@@ -12,6 +14,36 @@ interface Props {
   containerClasses?: string;
 }
 const GlobalSearch = ({ filters, otherClasses, containerClasses }: Props) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const query = searchParams.get("q");
+  const [search, setSearch] = useState(query || "");
+  const [isOpen, setIsOpen] = useState(false);
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (search) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "global",
+          value: search,
+        });
+
+        router.push(newUrl, { scroll: false });
+      } else {
+        if (query) {
+          const newUrl = removeKeysFromQuery({
+            params: searchParams.toString(),
+            keysToRemove: ["global", "type"],
+          });
+
+          router.push(newUrl, { scroll: false });
+        }
+      }
+    }, 300);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [search, router, pathname, searchParams, query]);
   return (
     <div
       className={`${containerClasses}relative w-full max-w-[600px] max-lg:hidden`}
@@ -30,11 +62,16 @@ const GlobalSearch = ({ filters, otherClasses, containerClasses }: Props) => {
         <Input
           type="text"
           placeholder="Search globally"
-          value=""
-          className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
-          onChange={() => {}}
+          value={search}
+          className="paragraph-regular no-focus placeholder text-dark400_light700 background-light800_darkgradient border-none shadow-none outline-none"
+          onChange={(e) => {
+            setSearch(e.target.value);
+            if (!isOpen) setIsOpen(true);
+            if (e.target.value === "") setIsOpen(false);
+          }}
         />
       </div>
+      {isOpen && <GlobalResult />}
     </div>
   );
 };
