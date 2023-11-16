5979e2131ae9ec1bde3cb638eb3e901dfa170517
Adam Gordon <adamg42685@gmail.com>
2023-11-16 02:40:38 -0500

    fixed text color in answers

--- components/shared/ParseHTML.tsx
+++ components/shared/ParseHTML.tsx
@@ -32,7 +32,7 @@ const ParseHTML = ({ data }: Props) => {
   useEffect(() => {
     Prism.highlightAll();
   }, []);
-  return <div>{parse(data)}</div>;
+  return <div className="text-dark300_light700">{parse(data)}</div>;
 };
 
 export default ParseHTML;
