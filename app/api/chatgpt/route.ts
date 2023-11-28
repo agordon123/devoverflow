9c4105f1d9eb464d20d51ad7bf300cef88b92535
adam <adamg42685@gmail.com>
2023-11-28 09:51:01 -0500

    another attempt at setting headers

--- app/api/chatgpt/route.ts
+++ app/api/chatgpt/route.ts
@@ -2,6 +2,10 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   const { question } = await req.json();
+  // Assuming 'req' is the incoming request and 'res' is the response you are preparing
+  const clientOrigin = req.headers.get("origin") || "http://localhost:3000";
+
+  // Set CORS headers in the response
 
   try {
     const response = await fetch("https://api.openai.com/v1/chat/completions", {
@@ -9,6 +13,7 @@ export async function POST(req: Request) {
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        "ACCESS-CONTROL-ALLOW-ORIGIN": clientOrigin,
       },
       body: JSON.stringify({
         model: "gpt-3.5-turbo",
@@ -26,7 +31,7 @@ export async function POST(req: Request) {
       }),
     });
     const data = await response.json();
-
+    console.log(JSON.stringify(response));
     const reply = data.choices[0].message.content;
 
     return NextResponse.json({ reply });
