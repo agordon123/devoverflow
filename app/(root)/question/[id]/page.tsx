5e075b5bf980a2aef082947d4a2f1fe9fee15b37
Adam Gordon <adamg42685@gmail.com>
2023-12-07 10:17:51 -0500

    modified question page for async function

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -45,18 +45,18 @@ const Page = async ({ params, searchParams }: any) => {
           <div className="flex justify-end">
             <Votes
               type="Question"
-              itemId={JSON.stringify(result._id)}
-              userId={JSON.stringify(mongoUser._id)}
-              upvotes={result.upvotes.length}
-              hasupVoted={result.upvotes.includes(mongoUser._id)}
-              downvotes={result.downvotes.length}
-              hasdownVoted={result.downvotes.includes(mongoUser._id)}
-              hasSaved={mongoUser?.saved.includes(result._id)}
+              itemId={JSON.stringify(result?._id)}
+              userId={JSON.stringify(mongoUser?._id)}
+              upvotes={result?.upvotes.length}
+              hasupVoted={result?.upvotes.includes(mongoUser?._id)}
+              downvotes={result?.downvotes.length}
+              hasdownVoted={result.downvotes.includes(mongoUser?._id)}
+              hasSaved={mongoUser?.saved.includes(result?._id)}
             />
           </div>
         </div>
         <h2 className="h2-semibold text-dark200_light900 mt-3.5 w-full text-left">
-          {result.title}
+          {result?.title}
         </h2>
       </div>
 
@@ -99,7 +99,7 @@ const Page = async ({ params, searchParams }: any) => {
 
       <AllAnswers
         questionId={result._id}
-        userId={mongoUser._id}
+        userId={mongoUser?._id}
         totalAnswers={result.answers.length}
         page={searchParams.page}
         filter={searchParams.filter}
@@ -108,7 +108,7 @@ const Page = async ({ params, searchParams }: any) => {
       <Answer
         question={result.content}
         questionId={JSON.stringify(result._id)}
-        authorId={JSON.stringify(mongoUser._id)}
+        authorId={JSON.stringify(mongoUser?._id)}
       />
     </>
   );
